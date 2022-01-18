2f9e9709af65e479697bd63203d48d03b9ce5f46
iamdavi <domato99.dom@gmail.com>
2022-01-18 16:47:41 +0100

    Init de vuelta al proyecto

---
+++ src/views/Editar.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h2>Editar</h2>
+        <form @submit.prevent="editarTarea(tarea)">
+            <input 
+                type="text"
+                v-model="tarea.nombre"
+            >
+            <button type="submit">Editar</button>
+        </form>
+    </div>
+</template>
+
+<script>
+import { mapActions, mapState } from "vuex";
+
+export default {
+    name: 'Editar',
+    data() {
+        return {
+            id: this.$route.params.id
+        }
+    },
+    created()  {
+        this.getTarea(this.id)
+    },
+    methods: {
+        ...mapActions(['getTarea', 'editarTarea'])
+    },
+    computed: {
+        ...mapState(['tarea'])
+    }
+}
+</script>
---
+++ src/views/Inicio.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h1>Inicio</h1>
+        <router-link to="/agregar">
+            <button>Agregar</button>
+        </router-link>
+        <ul>
+            <li 
+                v-for="(tarea, index) in tareas" :key="index"
+            >
+                {{ tarea.nombre }} - {{ tarea.id }}
+                <router-link 
+                    :to="{name: 'Editar', params: {id: tarea.id}}"
+                >
+                    <button>Editar</button>
+                </router-link>
+                <button @click="eliminarTarea(tarea.id)">Eliminar</button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import { mapActions, mapState } from "vuex";
+
+export default {
+    name: 'Inicio',
+    created() {
+        this.getTareas()
+    },
+    methods: {
+        ...mapActions(['getTareas', 'eliminarTarea'])
+    },
+    computed: {
+        ...mapState(['tareas'])
+    }
+}
+</script>
+
+<style>
+
+</style>
