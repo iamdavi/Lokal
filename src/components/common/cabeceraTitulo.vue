5e0b68bd380dc753b8a7328f3ddfada47dfe4691
iamdavi <domato99.dom@gmail.com>
2022-01-20 00:05:50 +0100

    Creado y añadido componente de cabecera

---
+++ src/components/common/cabeceraTitulo.vue
@@ -0,0 +1,19 @@
+<template>
+	<v-row justify="center">
+		<v-col cols="12" md="8" sm="12" class="text-centerf">
+			<h1>{{ titulo }}</h1>
+		</v-col>
+	</v-row>
+</template>
+
+<script>
+export default {
+	props: {
+		titulo: {
+			type: String,
+			required: true,
+			deault: 'Titulo'
+		}
+	}
+}
+</script>
--- src/views/persona/index.vue
+++ src/views/persona/index.vue
@@ -1,26 +1,28 @@
 <template>
-	<v-row justify="center">
-		<v-col cols="12" md="8" sm="12">
-			<h1>Personas</h1>
-		</v-col>
-		<v-col cols="12" md="8" sm="12">
-			<create-persona></create-persona>
-		</v-col>
-		<v-col cols="12" md="8" sm="12">
-			<list-persona></list-persona>
-		</v-col>
-	</v-row>
+	<div>
+		<cabecera-titulo titulo="Personas"></cabecera-titulo>
+		<v-row justify="center">
+			<v-col cols="12" md="8" sm="12">
+				<create-persona></create-persona>
+			</v-col>
+			<v-col cols="12" md="8" sm="12">
+				<list-persona></list-persona>
+			</v-col>
+		</v-row>
+	</div>
 </template>
 
 <script>
 import createPersona from '@/components/persona/createPersona'
 import listPersona from '@/components/persona/listPersona'
+import cabeceraTitulo from '@/components/common/cabeceraTitulo'
 
 export default {
 	name: 'Personas',
 	components: {
 		createPersona,
-		listPersona
+		listPersona,
+		cabeceraTitulo
 	}
 }
 </script>
